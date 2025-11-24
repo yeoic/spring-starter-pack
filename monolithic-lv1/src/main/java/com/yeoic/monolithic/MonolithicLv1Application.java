568949b524f9c8c4559e0cd08e25df308ae0d823
LoganYeo <118439555+yeoic@users.noreply.github.com>
2025-11-25 00:47:24 +0900

    init spring starter pack

---
+++ monolithic-lv1/src/main/java/com/yeoic/monolithic/MonolithicLv1Application.java
@@ -0,0 +1,13 @@
+package com.yeoic.monolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicLv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithicLv1Application.class, args);
+    }
+
+}
---
+++ monolithic-lv1/src/test/java/com/yeoic/monolithic/MonolithicLv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeoic.monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicLv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
